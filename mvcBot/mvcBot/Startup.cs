44789a8ae119b4c0e581be1c078d73cd0c38de24
xhaman <xhaman1@gmail.com>
2017-04-06 21:02:59 -0500

    Initial Build MVC whit .net 4.6

---
+++ mvcBot/mvcBot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcBot.Startup))]
+namespace mvcBot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
